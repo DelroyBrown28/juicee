34e2d0f33584866c2f3b7b2d9937f1577e6fc550
Delroy Brown <Delroy_jb@hotmail.co.uk>
2020-12-01 23:27:32 +0000

    carousel working

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,10 +1,11 @@
 $(document).ready(function () {
     $('.slickMe').slick({
+        nextArrow: $('#next-arrow'),
+        prevArrow: $('#previous-arrow'),
         slidesToShow: 1,
         slidesToScroll: 1,
         speed: 1000,
-        dots: true,
-        arrows: false,
+        arrows: true,
         cssEase: 'cubic-bezier(.9,.16,.07,1.06)', 
         infinite: false,
         centerMode: true,
@@ -20,5 +21,6 @@ $(document).ready(function () {
                 slidesToScroll: 1,
             }
         }]
-    })
+    });
+   
 })
